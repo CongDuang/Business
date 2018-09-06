a0f957dd98099dd1aef77e117045155fda70f64a
msp970818 <969017118@qq.com>
2018-09-06 10:34:13 +0800

    视频SDK模块

---
+++ SDK/src/main/java/net/msp/kaituo/sdk/BaseOkhttpTest.java
@@ -0,0 +1,21 @@
+package net.msp.kaituo.sdk;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Business
+ *
+ * @author kaituo:马世鹏
+ * @create 2018/9/6 10:28
+ * @since
+ **/
+public class BaseOkhttpTest extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    
+}
